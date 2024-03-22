e44dcabe027649cfe468250e1917dc0b4021c3c9
VulqyFR <vulqy.pro@gmail.com>
2024-03-22 20:07:52 +0100

    feat<routeur>: base of routing for the app

--- TKT/src/App.jsx
+++ TKT/src/App.jsx
@@ -1,4 +1,6 @@
 import { useContext, useEffect } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.jsx";
 import { ThemeContext } from "./context/ThemeContext.jsx";
 import "./stylesheets/main.scss";
 
@@ -8,7 +10,16 @@ function App() {
     document.body.classList.toggle("theme--dark", isDark);
     document.body.classList.toggle("theme--light", !isDark);
   }, [isDark]);
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Welcome to the homepage</h1>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
