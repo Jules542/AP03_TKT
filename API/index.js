66451c4df910dcbb1aa11585cb9ba588ac5bc1b0
ribeiroj <ribeiroj@domadj.fr>
2024-03-22 14:29:38 +0100

    initial-commit

---
+++ API/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRound= 10;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "tkt",
+});
+
+connection.connect((error) => {
+    if (error)
+    {
+        console.error("Erreur connexion BDD " + error.stack);
+        return;
+    }
+});
