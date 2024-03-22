9d6caf8bbefde292a165c8416b3fa539eb2db3ee
VulqyFR <vulqy.pro@gmail.com>
2024-03-22 14:58:53 +0100

    feat<api>: add listening port the the API

--- API/index.js
+++ API/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
-const mysql = require('mysql2');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const saltRound= 10;
+const jwt = require("jsonwebtoken");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 
@@ -12,16 +12,20 @@ app.use(cors());
 app.use(express.json());
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "tkt",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "tkt",
 });
 
 connection.connect((error) => {
-    if (error)
-    {
-        console.error("Erreur connexion BDD " + error.stack);
-        return;
-    }
+  if (error) {
+    console.error("Erreur connexion BDD " + error.stack);
+    return;
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
