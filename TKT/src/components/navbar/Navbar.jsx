04835456c56346c3adb44208490727da051be40e
VulqyFR <vulqy.pro@gmail.com>
2024-04-01 07:45:27 +0200

    feat<navbar>: styling navbar

--- TKT/src/components/navbar/Navbar.jsx
+++ TKT/src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
+import Moon from "../../assets/images/moon.svg";
+import Sun from "../../assets/images/sun.svg";
 import { ThemeContext } from "../../context/ThemeContext";
 import { UserContext } from "../../context/UserContext";
 
@@ -13,32 +15,36 @@ const Navbar = () => {
   }, [isDark]);
   return (
     <nav className="navbar">
-      <img src={Logo} alt="logo" />
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/attractions">Attractions</Link>
-        </li>
-        {isLoggedIn() && isAdmin() && (
+        <div className="navbar__left">
+          <img src={Logo} alt="logo" />
           <li>
-            <Link to="/affectations">Affectation de missions</Link>
+            <Link to="/">Home</Link>
           </li>
-        )}
-        <button onClick={toggleDark}>
-          {isDark ? "Switch to Light Mode" : "Switch to Dark Mode"}
-        </button>
-        {!isLoggedIn() && (
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/attractions">Attractions</Link>
           </li>
-        )}
-        {isLoggedIn() && (
-          <li>
-            <button onClick={handleLogout}>Logout</button>
-          </li>
-        )}
+          {isLoggedIn() && isAdmin() && (
+            <li>
+              <Link to="/affectations">Affectation de missions</Link>
+            </li>
+          )}
+        </div>
+        <div className="navbar__right">
+          <a onClick={toggleDark}>
+            {isDark ? <img src={Sun} /> : <img src={Moon} />}
+          </a>
+          {!isLoggedIn() && (
+            <li className="navbar__log">
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+          {isLoggedIn() && (
+            <li className="navbar__log">
+              <a onClick={handleLogout}>Logout</a>
+            </li>
+          )}
+        </div>
       </ul>
     </nav>
   );
