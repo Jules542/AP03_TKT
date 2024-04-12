47d5fe6423c8b675a13ff9d539879caefc526ee4
Jules <ribeirojules78@gmail.com>
2024-04-12 17:21:48 +0200

    feature<homePage>: Création de la page et de la route vers la page

---
+++ TKT/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useEffect, useState } from "react";
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>PAGE D'ACCUEIL</h1>
+        </div>
+    )
+}
+
+export default HomePage;
