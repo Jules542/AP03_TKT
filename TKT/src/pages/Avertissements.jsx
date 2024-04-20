563ebf10566c062db30a32e22f057d069269de32
Sami Inan <inansami.05@gmail.com>
2024-04-20 17:27:21 +0200

    feat<fix-avertissement-not-logged-in-no-admin>: Ajustement de la modification, suprression et ajout d'un avertissement seulement pour l'admin et de l'affichage des avertissements pour tous.

--- TKT/src/pages/Avertissements.jsx
+++ TKT/src/pages/Avertissements.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Avertissement from "../components/avertissements/Avertissement";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
 
 const Avertissements = () => {
+  const { isLoggedIn, isAdmin } = useContext(UserContext);
   const [avertissements, setAvertissements] = useState([]);
   const [users, setUsers] = useState([]);
   const [niveaux, setNiveaux] = useState([]);
@@ -21,7 +24,7 @@ const Avertissements = () => {
         setNiveaux(response.data);
       });
     axios
-      .get("http://localhost:3000/users", {
+      .get("http://localhost:3000/usersNoAdmin", {
         headers: {
           "Content-Type": "application/json",
           "x-access-token": `${token}`,
@@ -31,12 +34,7 @@ const Avertissements = () => {
         setUsers(response.data);
       });
     axios
-      .get("http://localhost:3000/avertissements", {
-        headers: {
-          "Content-Type": "application/json",
-          "x-access-token": `${token}`,
-        },
-      })
+      .get("http://localhost:3000/avertissements")
       .then((response) => {
         setAvertissements(response.data);
       });
@@ -53,7 +51,9 @@ const Avertissements = () => {
           <p>Accueil &gt; Avertissements</p>
           <h1>Avertissements</h1>
         </div>
-        <Link to="/avertissements/nouveau" className="avertissement-new">Nouvelle</Link>
+        {isLoggedIn() && isAdmin() ? (
+          <Link to="/avertissements/nouveau" className="avertissement-new">Nouveau</Link>
+        ) : null}
         <div className="avertissements-wrapper-list">
         {avertissements.map((avertissement) => (
           <Avertissement
